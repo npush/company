5860bc113eeb5ce0c2b127082cd0385ca63a0ea4
nick <nikolay.pushkar@gmail.com>
2016-12-05 12:41:29 +0200

    Disable cache in sub category list

---
+++ app/code/local/Stableflow/AdditionalCmsPageLayouts/Block/Navigation.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by nick
+ * Project magento1.dev
+ * Date: 12/5/16
+ * Time: 12:14 PM
+ */
+class Stableflow_AdditionalCmsPageLayouts_Block_Navigation extends Mage_Catalog_Block_Navigation{
+
+    /**
+     * Disable cache in block ...
+     */
+    protected function _construct(){
+
+    }
+}
