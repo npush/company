bd0d981628311d7b9f04350acf7db3a0ffaf11d7
nick <nikolay.pushkar@gmail.com>
2016-12-15 11:33:10 +0200

    Block article

--- app/code/local/Mageplaza/BetterBlog/Block/Post/Relation.php
+++ app/code/local/Mageplaza/BetterBlog/Block/Post/Relation.php
@@ -6,10 +6,19 @@
  * Date: 12/14/16
  * Time: 1:22 PM
  */
+
 class Mageplaza_BetterBlog_Block_Post_Relation extends Mage_Core_Block_Template{
+
+    protected $_postIds;
+    protected $_posts;
+    protected $_product;
+    protected $_categoryId;
 
     public function _construct(){
         parent::_construct();
+        $this->_categoryId = $this->getProduct()->getCategoryId();
+        $this->_postIds = Mage::getResourceModel('mageplaza_betterblog/post_relation')
+            ->getRelatedPostIds($this->_categoryId);
     }
 
     protected function _prepareLayout(){
@@ -18,17 +27,20 @@ class Mageplaza_BetterBlog_Block_Post_Relation extends Mage_Core_Block_Template{
     }
 
     public function getPosts(){
-        $_categoryId = $this->getProduct()->getCategoryId();
-        $postIds = Mage::getResourceModel('mageplaza_betterblog/post_relation')->getRelatedPostIds($_categoryId);
-        $posts = Mage::getResourceModel('mageplaza_betterblog/post_collection')
-            ->setStoreId(Mage::app()->getStore()->getId())
-            ->addAttributeToSelect('*')
-            ->addAttributeToFilter('status', 1)
-            ->addAttributeToFilter('entity_id',$postIds);
-        return $posts;
+        if(is_null($this->_posts)) {
+            $this->_posts = Mage::getResourceModel('mageplaza_betterblog/post_collection')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->addAttributeToSelect('*')
+                ->addAttributeToFilter('status', 1)
+                ->addAttributeToFilter('entity_id', $this->_postIds);
+        }
+        return $this->_posts;
     }
 
     public function getProduct(){
-        return Mage::registry('current_product');
+        if(is_null($this->_product)) {
+            $this->_product = Mage::registry('current_product');
+        }
+        return $this->_product;
     }
 }
