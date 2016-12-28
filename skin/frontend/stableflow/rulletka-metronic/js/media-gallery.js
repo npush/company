7702304f13e51a430d573a0fdeb820aa0d59f914
nick <nikolay.pushkar@gmail.com>
2016-12-28 18:41:09 +0200

    change media block

---
+++ skin/frontend/stableflow/rulletka-metronic/js/media-gallery.js
@@ -0,0 +1,133 @@
+/**
+ * Created by nick on 12/28/16.
+ */
+// ==============================================
+// PDP - image zoom - needs to be available outside document.ready scope
+// ==============================================
+
+var ProductMediaManager = {
+    IMAGE_ZOOM_THRESHOLD: 20,
+    imageWrapper: null,
+
+    destroyZoom: function() {
+        $j('.zoomContainer').remove();
+        $j('.product-image-gallery .gallery-image').removeData('elevateZoom');
+    },
+
+    createZoom: function(image) {
+        // Destroy since zoom shouldn't be enabled under certain conditions
+        ProductMediaManager.destroyZoom();
+
+        if(
+            // Don't use zoom on devices where touch has been used
+        PointerManager.getPointer() == PointerManager.TOUCH_POINTER_TYPE
+            // Don't use zoom when screen is small, or else zoom window shows outside body
+        || Modernizr.mq("screen and (max-width:" + bp.medium + "px)")
+        ) {
+            return; // zoom not enabled
+        }
+
+        if(image.length <= 0) { //no image found
+            return;
+        }
+
+        if(image[0].naturalWidth && image[0].naturalHeight) {
+            var widthDiff = image[0].naturalWidth - image.width() - ProductMediaManager.IMAGE_ZOOM_THRESHOLD;
+            var heightDiff = image[0].naturalHeight - image.height() - ProductMediaManager.IMAGE_ZOOM_THRESHOLD;
+
+            if(widthDiff < 0 && heightDiff < 0) {
+                //image not big enough
+
+                image.parents('.product-image').removeClass('zoom-available');
+
+                return;
+            } else {
+                image.parents('.product-image').addClass('zoom-available');
+            }
+        }
+
+        image.elevateZoom();
+    },
+
+    swapImage: function(targetImage) {
+        targetImage = $j(targetImage);
+        targetImage.addClass('gallery-image');
+
+        ProductMediaManager.destroyZoom();
+
+        var imageGallery = $j('.product-image-gallery');
+
+        if(targetImage[0].complete) { //image already loaded -- swap immediately
+
+            imageGallery.find('.gallery-image').removeClass('visible');
+
+            //move target image to correct place, in case it's necessary
+            imageGallery.append(targetImage);
+
+            //reveal new image
+            targetImage.addClass('visible');
+
+            //wire zoom on new image
+            ProductMediaManager.createZoom(targetImage);
+
+        } else { //need to wait for image to load
+
+            //add spinner
+            imageGallery.addClass('loading');
+
+            //move target image to correct place, in case it's necessary
+            imageGallery.append(targetImage);
+
+            //wait until image is loaded
+            imagesLoaded(targetImage, function() {
+                //remove spinner
+                imageGallery.removeClass('loading');
+
+                //hide old image
+                imageGallery.find('.gallery-image').removeClass('visible');
+
+                //reveal new image
+                targetImage.addClass('visible');
+
+                //wire zoom on new image
+                ProductMediaManager.createZoom(targetImage);
+            });
+
+        }
+    },
+
+    wireThumbnails: function() {
+        //trigger image change event on thumbnail click
+        $j('.product-image-thumbs .thumb-link').click(function(e) {
+            e.preventDefault();
+            var jlink = $j(this);
+            var target = $j('#image-' + jlink.data('image-index'));
+
+            ProductMediaManager.swapImage(target);
+        });
+    },
+
+    initZoom: function() {
+        ProductMediaManager.createZoom($j(".gallery-image.visible")); //set zoom on first image
+    },
+
+    init: function() {
+        ProductMediaManager.imageWrapper = $j('.product-img-box');
+
+        // Re-initialize zoom on viewport size change since resizing causes problems with zoom and since smaller
+        // viewport sizes shouldn't have zoom
+        $j(window).on('delayed-resize', function(e, resizeEvent) {
+            ProductMediaManager.initZoom();
+        });
+
+        ProductMediaManager.initZoom();
+
+        ProductMediaManager.wireThumbnails();
+
+        $j(document).trigger('product-media-loaded', ProductMediaManager);
+    }
+};
+
+$j(document).ready(function() {
+    ProductMediaManager.init();
+});
