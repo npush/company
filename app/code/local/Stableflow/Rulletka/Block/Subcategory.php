c439fcd2118cfc3da4d037ad44d04f4b7ce71827
nick <nikolay.pushkar@gmail.com>
2017-01-12 18:22:29 +0200

    ...

---
+++ app/code/local/Stableflow/Rulletka/Block/Subcategory.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Created by nick
+ * Project magento1.dev
+ * Date: 1/12/17
+ * Time: 12:12 PM
+ */
+
+class  Stableflow_Rulletka_Block_Subcategory extends Mage_Core_Block_Abstract {
+
+    /**
+     * categoryTree data tree
+     *
+     * @var Varien_Data_Tree_Node
+     */
+    protected $_categoryTree;
+
+    protected $_templateFile;
+
+    public function _construct(){
+        $this->_categoryTree = new Varien_Data_Tree_Node(array(), 'root', new Varien_Data_Tree());
+        /*
+        * setting cache to save the topmenu block
+        */
+        $this->setCacheTags(array('catalog_category_tree'));
+        $this->setCacheLifetime(false);
+    }
+
+    protected function _renderCategoryTree($category){
+
+    }
+
+    /**
+     * Renders block html
+     * @return string
+     * @throws Exception
+     */
+    protected function _toHtml()
+    {
+        $this->_addCacheTags();
+        $menuTree = $this->getMenuTree();
+        $childrenWrapClass = $this->getChildrenWrapClass();
+        if (!$this->getTemplate() || is_null($menuTree) || is_null($childrenWrapClass)) {
+            throw new Exception("Top-menu renderer isn't fully configured.");
+        }
+
+        $includeFilePath = realpath(Mage::getBaseDir('design') . DS . $this->getTemplateFile());
+        if (strpos($includeFilePath, realpath(Mage::getBaseDir('design'))) === 0 || $this->_getAllowSymlinks()) {
+            $this->_templateFile = $includeFilePath;
+        } else {
+            throw new Exception('Not valid template file:' . $this->_templateFile);
+        }
+        return $this->render($menuTree, $childrenWrapClass);
+    }
+
+    /**
+     * Fetches template. If template has return statement, than its value is used and direct output otherwise.
+     * @param Varien_Data_Tree_Node $menuTree
+     * @param $childrenWrapClass
+     * @return string
+     */
+    public function render(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
+    {
+        ob_start();
+        $html = include $this->_templateFile;
+        $directOutput = ob_get_clean();
+
+        if (is_string($html)) {
+            return $html;
+        } else {
+            return $directOutput;
+        }
+    }
+
+    /**
+     * Recursively adds categories
+     *
+     * @param Varien_Data_Tree_Node_Collection|array $categories
+     * @param Varien_Data_Tree_Node $parentCategoryNode
+     * @param Mage_Page_Block_Html_Topmenu $menuBlock
+     * @param bool $addTags
+     */
+    protected function _addCategoriesToMenu($categories, $parentCategoryNode, $menuBlock, $addTags = false)
+    {
+        $categoryModel = Mage::getModel('catalog/category');
+        foreach ($categories as $category) {
+            if (!$category->getIsActive()) {
+                continue;
+            }
+
+            $nodeId = 'category-node-' . $category->getId();
+
+            $categoryModel->setId($category->getId());
+            if ($addTags) {
+                $menuBlock->addModelTags($categoryModel);
+            }
+
+            $tree = $parentCategoryNode->getTree();
+            $categoryData = array(
+                'name' => $category->getName(),
+                'id' => $nodeId,
+                'url' => Mage::helper('catalog/category')->getCategoryUrl($category),
+                'is_active' => $this->_isActiveMenuCategory($category),
+                'product_count' => $category->getProductCount(),
+            );
+            $categoryNode = new Varien_Data_Tree_Node($categoryData, 'id', $tree, $parentCategoryNode);
+            $parentCategoryNode->addChild($categoryNode);
+
+            $subcategories = $category->getChildren();
+
+            $this->_addCategoriesToMenu($subcategories, $categoryNode, $menuBlock, $addTags);
+        }
+    }
+
+    /**
+     * Add cache tags
+     *
+     * @return void
+     */
+    protected function _addCacheTags()
+    {
+        $parentBlock = $this->getParentBlock();
+        if ($parentBlock) {
+            $this->addCacheTag($parentBlock->getCacheTags());
+        }
+    }
+
+    /**
+     * Retrieve cache key data
+     *
+     * @return array
+     */
+    public function getCacheKeyInfo(){
+        $shortCacheId = array(
+            'TOPMENU',
+            Mage::app()->getStore()->getId(),
+            Mage::getDesign()->getPackageName(),
+            Mage::getDesign()->getTheme('template'),
+            Mage::getSingleton('customer/session')->getCustomerGroupId(),
+            'template' => $this->getTemplate(),
+            'name' => $this->getNameInLayout(),
+            $this->getCurrentEntityKey()
+        );
+        $cacheId = $shortCacheId;
+
+        $shortCacheId = array_values($shortCacheId);
+        $shortCacheId = implode('|', $shortCacheId);
+        $shortCacheId = md5($shortCacheId);
+
+        $cacheId['entity_key'] = $this->getCurrentEntityKey();
+        $cacheId['short_cache_id'] = $shortCacheId;
+
+        return $cacheId;
+    }
+
+    /**
+     * Retrieve current entity key
+     *
+     * @return int|string
+     */
+    public function getCurrentEntityKey(){
+        if (null === $this->_currentEntityKey) {
+            $this->_currentEntityKey = Mage::registry('current_entity_key')
+                ? Mage::registry('current_entity_key') : Mage::app()->getStore()->getRootCategoryId();
+        }
+        return $this->_currentEntityKey;
+    }
+}
