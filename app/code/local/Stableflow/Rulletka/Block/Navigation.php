33cfcd844c81c569b99c85494d10dff0cda593fe
nick <nikolay.pushkar@gmail.com>
2017-01-27 10:46:44 +0200

    ...

--- app/code/local/Stableflow/Rulletka/Block/Navigation.php
+++ app/code/local/Stableflow/Rulletka/Block/Navigation.php
@@ -21,7 +21,7 @@ class Stableflow_Rulletka_Block_Navigation extends Mage_Catalog_Block_Navigation
         $productCollection = Mage::getResourceModel('catalog/product_collection');
         $productCollection->addCountToCategories($cat);
         foreach($cat as $_childId) {
-            $childCategory = $_childId; //Mage::getModel('catalog/category')->load($_childId->getId());
+            $childCategory = Mage::getModel('catalog/category')->load($_childId->getId());
 
             if ($childCategory->getIsActive()) {
                 $text .= '<li><a href="' . $childCategory->getUrlPath() . '">' . $this->htmlEscape($childCategory->getName()) . '</a><span> (' . $childCategory->getProductCount() . ')</span></li>';
