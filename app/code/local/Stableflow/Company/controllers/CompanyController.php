6eb8dbf0c9a7422ce86d5a94985421f4d3cdd6e2
nick <nick_push@mail.ru>
2017-03-05 23:10:48 +0200

    ,,,

--- app/code/local/Stableflow/Company/controllers/CompanyController.php
+++ app/code/local/Stableflow/Company/controllers/CompanyController.php
@@ -24,8 +24,8 @@ class Stableflow_Company_CompanyController extends Mage_Core_Controller_Front_Ac
     public function viewAction(){
         $isAjax = Mage::app()->getRequest()->getParam('is_ajax');
         if($isAjax){
-            die('asdasdas');
             $this->loadLayout();
+            die('asdas');
             $myBlock = $this->getLayout()->getBlock('company_product_list');
             $myHtml =  $myBlock->toHtml(); //also consider $myBlock->renderView();
             $this->getResponse()
