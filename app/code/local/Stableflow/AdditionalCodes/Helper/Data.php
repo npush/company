bfd0fae4157d346d6c0f324f46d6cbb7e34d0e2f
nick <nikolay.pushkar@gmail.com>
2017-03-23 18:36:50 +0200

    ...

---
+++ app/code/local/Stableflow/AdditionalCodes/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by nick
+ * Project magento1.dev
+ * Date: 3/23/17
+ * Time: 6:36 PM
+ */
+class Stableflow_AdditionalCodes_Helper_Data extends Mage_Core_Helper_Data
+{
+
+}
