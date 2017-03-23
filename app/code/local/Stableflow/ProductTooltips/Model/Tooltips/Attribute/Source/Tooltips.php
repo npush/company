0986350b3fd014a48026476033492affdda1dd24
nick <nikolay.pushkar@gmail.com>
2017-03-23 13:25:59 +0200

    ...

--- app/code/local/Stableflow/ProductTooltips/Model/Tooltips/Attribute/Source/Tooltips.php
+++ app/code/local/Stableflow/ProductTooltips/Model/Tooltips/Attribute/Source/Tooltips.php
@@ -25,22 +25,6 @@ class Stableflow_ProductTooltips_Model_Tooltips_Attribute_Source_Tooltips extend
         return $this->_options;
     }
 
-    public function getOptionText($value)
-    {
-        $options = $this->getAllOptions();
-        // Fixed for tax_class_id and custom_design
-        if (sizeof($options) > 0) foreach($options as $option) {
-            if (isset($option['tooltip_id']) && $option['tooltip_id'] == $value) {
-                return isset($option['tooltip_id']) ? $option['tooltip_id'] : $option['tooltip_id'];
-            }
-        } // End
-        if (isset($options[$value])) {
-            return $options[$value];
-        }
-        return false;
-    }
-
-
     public function getTooltips(){
         if(is_null($this->_tooltips)) {
             $this->_tooltips = Mage::getModel('product_tooltips/tooltip')
