498172d918e2a5f0d9edcaf4750c94b506037556
nick <nikolay.pushkar@gmail.com>
2017-04-14 17:20:39 +0300

    ...

--- shell/resave.php
+++ shell/resave.php
@@ -12,13 +12,15 @@ class Resave_models extends Mage_Shell_Abstract
 {
     public function run()
     {
-        $collection = Mage::getModel('company/company')->getCollection()
+        $collection = Mage::getModel('company/company')
+            ->getCollection()
             ->addAttributeToSelect('id');
 
         foreach ($collection as $model) {
             $id = $model->getId();
             printf("%s \n", $id);
-            $model = Mage::getModel('company/company')->load($id);
+            $model = Mage::getModel('company/company')
+                ->load($id);
             $model->setData('updated_at', Mage::getSingleton('core/date')->gmtDate());
             $model->save($id);
         }
