e8909c848c3fc3de519f92b543a6c86cf210418a
nick <nikolay.pushkar@gmail.com>
2017-04-18 16:06:42 +0300

    ...

--- app/code/local/Stableflow/Autosuggest/controllers/AjaxController.php
+++ app/code/local/Stableflow/Autosuggest/controllers/AjaxController.php
@@ -6,6 +6,7 @@
  * Date: 1/25/17
  * Time: 1:11 PM
  */
+
 class Stableflow_Autosuggest_AjaxController extends Mage_Core_Controller_Front_Action{
 
 
@@ -14,10 +15,49 @@ class Stableflow_Autosuggest_AjaxController extends Mage_Core_Controller_Front_A
         $this->renderLayout();
     }
 
-    public function searchAction(){
+    public function _searchAction(){
         $this->loadLayout();
 
         $this->getLayout()->getBlock('root')->search();
         $this->renderLayout();
+    }
+
+    public function searchAction(){
+        $query = Mage::helper('catalogsearch')->getQuery();
+        /* @var $query Mage_CatalogSearch_Model_Query */
+
+        $query->setStoreId(Mage::app()->getStore()->getId());
+
+        if ($query->getQueryText() != '') {
+            if (Mage::helper('catalogsearch')->isMinQueryLength()) {
+                $query->setId(0)
+                    ->setIsActive(1)
+                    ->setIsProcessed(1);
+            } else {
+                if ($query->getId()) {
+                    $query->setPopularity($query->getPopularity() + 1);
+                } else {
+                    $query->setPopularity(1);
+                }
+
+                if ($query->getRedirect()) {
+                    $query->save();
+                    $this->getResponse()->setRedirect($query->getRedirect());
+                    return;
+                } else {
+                    $query->prepare();
+                }
+            }
+
+            Mage::helper('catalogsearch')->checkNotes();
+
+            $this->loadLayout();
+            $this->getLayout()->getBlock('root')->search();
+            $this->renderLayout();
+
+            if (!Mage::helper('catalogsearch')->isMinQueryLength()) {
+                $query->save();
+            }
+        }
     }
 }
