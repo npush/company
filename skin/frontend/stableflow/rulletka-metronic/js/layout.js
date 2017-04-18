f6124e6d78de25e43290f71113aa8e7d9e8d074e
Lara Zhuga <larazhuga88888@gmail.com>
2017-04-18 12:09:29 +0300

    st

--- skin/frontend/stableflow/rulletka-metronic/js/layout.js
+++ skin/frontend/stableflow/rulletka-metronic/js/layout.js
@@ -45,7 +45,7 @@ var Layout = function ($) {
                     height = App.getViewPort().height - headerHeight;
                 }
             }
-            content.attr('style', 'min-height:' + height - 80 + 'px');
+            content.attr('style', 'min-height:' + height + 'px');
         }
     };
 
