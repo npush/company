58b347aa605081b9db8e56317b434e41b037956d
nick <nikolay.pushkar@gmail.com>
2017-04-26 13:19:57 +0300

    ...

--- skin/frontend/stableflow/default/quick_contact/js/quickContact.js
+++ skin/frontend/stableflow/default/quick_contact/js/quickContact.js
@@ -1,25 +1,27 @@
 jQuery(document).ready(function(){
 	jQuery('#contactForm').submit(function(){
-		var action = jQuery(this).attr('action');
-		jQuery('.ajax-contact-overlay').fadeIn(200, function(){
-			jQuery('.ajax-contact-loader').show();
-		});
-		jQuery.post(action, {
-				name: jQuery('#name').val(),
-				email: jQuery('#email').val(),
-				telephone: jQuery('#telephone').val(),
-				comment: jQuery('#comment').val(),
-				product_name:jQuery('#product_name').val()
-			}, function(data){
-				jQuery('#contactForm #submit').attr('disabled','true');
-				jQuery('.response').remove();
-				jQuery('#contactForm').before(data);
-				jQuery('.ajax-contact-loader').hide();
-				jQuery('.ajax-contact-overlay').fadeOut(200);
-				jQuery('.response').slideDown();
+		if(contactForm.validator.validate()) {
+			var action = jQuery(this).attr('action');
+			jQuery('.ajax-contact-overlay').fadeIn(200, function () {
+				jQuery('.ajax-contact-loader').show();
+			});
+			jQuery.post(action, {
+					name: jQuery('#name').val(),
+					email: jQuery('#email').val(),
+					telephone: jQuery('#telephone').val(),
+					comment: jQuery('#comment').val(),
+					product_name: jQuery('#product_name').val()
+				}, function (data) {
+					jQuery('#contactForm #submit').attr('disabled', 'true');
+					jQuery('.response').remove();
+					jQuery('#contactForm').before(data);
+					jQuery('.ajax-contact-loader').hide();
+					jQuery('.ajax-contact-overlay').fadeOut(200);
+					jQuery('.response').slideDown();
 
-			}
-		);
-		return false;
+				}
+			);
+			return false;
+		}
 	});
 });
