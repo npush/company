b01dae9de84707c5de702cfd24601919f7c3b446
nick <nikolay.pushkar@gmail.com>
2017-05-11 15:36:35 +0300

    ...

--- app/code/local/Stableflow/Company/Block/Adminhtml/Company/Edit/Tab/Owner.php
+++ app/code/local/Stableflow/Company/Block/Adminhtml/Company/Edit/Tab/Owner.php
@@ -6,10 +6,118 @@
  * Date: 12/22/16
  * Time: 6:02 PM
  */
-class Stableflow_Company_Block_Adminhtml_Company_Edit_Tab_Owner extends Mage_Adminhtml_Block_Widget_Form{
+class Stableflow_Company_Block_Adminhtml_Company_Edit_Tab_Owner extends Mage_Adminhtml_Block_Widget_Grid{
 
-    public function __construct(){
+    /*public function __construct(){
         parent::__construct();
         $this->setTemplate('company/tab/owner.phtml');
+    }*/
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('company_owners_grid');
+        $this->setDefaultSort('entity_id');
+        $this->setUseAjax(true);
+    }
+
+    public function getCompany()
+    {
+        return Mage::registry('current_company');
+    }
+
+    protected function _addColumnFilterToCollection($column)
+    {
+        // Set custom filter for in category flag
+        if ($column->getId() == 'in_category') {
+            $productIds = $this->_getSelectedProducts();
+            if (empty($productIds)) {
+                $productIds = 0;
+            }
+            if ($column->getFilter()->getValue()) {
+                $this->getCollection()->addFieldToFilter('entity_id', array('in'=>$productIds));
+            }
+            elseif(!empty($productIds)) {
+                $this->getCollection()->addFieldToFilter('entity_id', array('nin'=>$productIds));
+            }
+        }
+        else {
+            parent::_addColumnFilterToCollection($column);
+        }
+        return $this;
+    }
+
+    protected function _prepareCollection()
+    {
+        if ($this->getCompany()->getId()) {
+            $this->setDefaultFilter(array('in_category'=>1));
+        }
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('price')
+            ->addStoreFilter($this->getRequest()->getParam('store'))
+            ->joinField('position',
+                'catalog/category_product',
+                'position',
+                'product_id=entity_id',
+                'category_id='.(int) $this->getRequest()->getParam('id', 0),
+                'left');
+        $this->setCollection($collection);
+
+        if ($this->getCompany()->getIsReadonly()) {
+            $productIds = $this->_getSelectedProducts();
+            if (empty($productIds)) {
+                $productIds = 0;
+            }
+            $this->getCollection()->addFieldToFilter('entity_id', array('in'=>$productIds));
+        }
+
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+        if (!$this->getCompany()->getIsReadonly()) {
+            $this->addColumn('in_category', array(
+                'header_css_class' => 'a-center',
+                'type'      => 'checkbox',
+                'name'      => 'in_category',
+                'values'    => $this->_getSelectedProducts(),
+                'align'     => 'center',
+                'index'     => 'entity_id'
+            ));
+        }
+        $this->addColumn('entity_id', array(
+            'header'    => Mage::helper('catalog')->__('ID'),
+            'sortable'  => true,
+            'width'     => '60',
+            'index'     => 'entity_id'
+        ));
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('catalog')->__('Name'),
+            'index'     => 'name'
+        ));
+        $this->addColumn('sku', array(
+            'header'    => Mage::helper('catalog')->__('Email'),
+            'width'     => '80',
+            'index'     => 'sku'
+        ));
+        return parent::_prepareColumns();
+    }
+
+    public function getGridUrl()
+    {
+        return $this->getUrl('*/*/grid', array('_current'=>true));
+    }
+
+    protected function _getSelectedProducts()
+    {
+        $products = $this->getRequest()->getPost('selected_products');
+        if (is_null($products)) {
+            $products = $this->getCompany()->getOwnerPosition();
+            return array_keys($products);
+        }
+        return $products;
     }
 }
---
+++ app/code/local/Stableflow/Company/Block/Adminhtml/Company/Helper/OwnerGrid.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by nick
+ * Project magento1.dev
+ * Date: 5/11/17
+ * Time: 12:37 PM
+ */
+class OwnerGrid
+{
+
+}
