68cda98299b1858ff661b4b06e4fd533783f34d3
nick <nikolay.pushkar@gmail.com>
2017-11-14 18:31:55 +0200

    Company module Price parser

--- app/code/local/Stableflow/Company/Model/Parser/Adapter/Xlsx.php
+++ app/code/local/Stableflow/Company/Model/Parser/Adapter/Xlsx.php
@@ -9,4 +9,26 @@
 class Stableflow_Company_Model_Parser_Adapter_Xlsx extends Stableflow_Company_Model_Parser_Adapter_Xls
 {
 
+    /**
+     * array = array(
+     * 'A' => value
+     * 'B' => value
+     * ....
+     * )
+     * @return array
+     */
+    protected function _getRow()
+    {
+        $rowData = array();
+        $row = $this->_rowIterator->current();
+        $cellIterator = $row->getCellIterator();
+        // Iterate only on
+        $cellIterator->setIterateOnlyExistingCells(true);
+        /** @var PHPExcel_Cell $cell */
+        foreach($cellIterator as $cell){
+            $format = (string)$cell->getStyle()->getNumberFormat()->getFormatCode();
+            $rowData[$cell->getColumn()] = $cell->getFormattedValue();
+        }
+        return $rowData;
+    }
 }
